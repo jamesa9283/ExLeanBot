4f0d32ffc497b02665cbdd7ddb3b6e53b24ee88b
James Arthur <jamesarthur7@icloud.com>
2021-06-13 16:58:23 +0100

    added working roles

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const Discord = require('discord.js');
 const { Client, MessageEmbed } = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 client.login(process.env.DISCORD_TOKEN)
 
@@ -21,40 +21,63 @@ client.on('ready', () => {
     }
   });
 
-  client.on("message", async message => {
-    // Checking if the message author is a bot.
-    if (message.author.bot) return false;
+  client.on('message', msg => {
+  if (msg.content.toLowerCase().includes('ping pong')) {
+    msg.react('😄');
+  }
+  });
 
-    // Getting the role by ID.
-    const Role1 = message.guild.roles.cache.get("853628984415748146");
 
-    // Creating a filter.
-    const Filter = (reaction, user) => user.id == message.author.id;
-
-    // Creating the embed message.
-    const Embed = new Discord.MessageEmbed()
-        .setDescription(`Choose a role: ${Role1.name}`)
-    
-    // Awaiting for the embed message to be sent.
-    const reactionMessage = await message.channel.send(Embed);
-
-    // Reacting to the embed message.
-    await reactionMessage.react("😎");
-
-    // Awaiting a reaction to the embed message. Time is measured in ms. (30000 ms - 30 seconds)
-    reactionMessage.awaitReactions(Filter, {max: 1, time: 30000, errors: ["time"]}).then(collected => {
-        // Getting the first reaction in the collection.
-        const reaction = collected.first();
-        
-        // Creating a switch statement for reaction.emoji.name.
-        switch (reaction.emoji.name) {
-            case "😎":
-                // Checking if the member already has the role.
-                if (message.member.roles.cache.has(Role1.id)) {return message.channel.send("You already have the role.")};
-                // Adding the role.
-                message.member.roles.add(Role1).then(message.channel.send("Role added!"));
-                // Breaking the switch statement to make sure no other cases are executed.
-                break
+  client.on('messageReactionAdd', async (reaction, user) => {
+    const TT = reaction.message.guild.roles.cache.find(guild => guild.id === '853628984415748146');
+    const SH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630661265195028');
+    const HH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630766746435585');
+    const other = reaction.message.guild.roles.cache.find(guild => guild.id === '853630826284449803');
+    // const TT = reaction.guild.roles.cache.get("853628984415748146");
+    // const SH = reaction.guild.roles.cache.get("853630661265195028");
+    // const HH = reaction.guild.roles.cache.get("853630766746435585");
+    // const other = reaction.guild.roles.cache.get("853630826284449803");
+	// When a reaction is received, check if the structure is partial
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+    if (reaction.message.author.id == '853630661265195028') {
+        if (reaction._emoji.name == '1️⃣') {
+            console.log(user)
+            reaction.message.guild.members.fetch(user.id).then(member => {
+                member.roles.add(TT).catch(console.error);
+            })
         }
-    })
+    }
+    if (reaction.message.author.id == '853624054886694932') {
+        if (reaction._emoji.name == '2️⃣') {
+            console.log(user)
+            reaction.message.guild.members.fetch(user.id).then(member => {
+                member.roles.add(SH).catch(console.error);
+            })
+        }
+    }
+    if (reaction.message.author.id == '853630766746435585') {
+        if (reaction._emoji.name == '3️⃣') {
+            console.log(user)
+            reaction.message.guild.members.fetch(user.id).then(member => {
+                member.roles.add(HH).catch(console.error);
+            })
+        }
+    }
+    if (reaction.message.author.id == '853630826284449803') {
+        if (reaction._emoji.name == '4️⃣') {
+            console.log(user)
+            reaction.message.guild.members.fetch(user.id).then(member => {
+                member.roles.add(other).catch(console.error);
+            })
+        }
+    }
 });
