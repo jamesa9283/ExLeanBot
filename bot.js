190b29b7bef59531d72fed3f2604fe8d279f0838
Ellie <37948990+Elisiah@users.noreply.github.com>
2021-06-14 01:29:40 +0100

    easier removal of roles
    roles can be removed the same way they are added now since in the newer discord js versions the cached message handles ReactionManager

--- bot.js
+++ bot.js
@@ -17,36 +17,33 @@ client.login(process.env.DISCORD_TOKEN)
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-  });
+});
   
-  client.on('message', msg => {
+client.on('message', msg => {
     if (msg.content.toLowerCase().includes('ping')) {
       msg.channel.send('Pong!');
     }
-  });
+});
 
-  client.on('message', msg => {
+client.on('message', msg => {
     if (msg.content.toLowerCase().includes('beep')) {
       msg.channel.send('BOOP!');
     }
-  });
+});
 
-  client.on('message', msg => {
+client.on('message', msg => {
   if (msg.content.toLowerCase().includes('ping pong')) {
     msg.react('😄');
   }
-  });
+});
 
-
-  client.on('messageReactionAdd', async (reaction, user) => {
+client.on('messageReactionAdd', async (reaction, user) => {
+    // cached roles to be added
     const TT = reaction.message.guild.roles.cache.find(guild => guild.id === '853628984415748146');
     const SH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630661265195028');
     const HH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630766746435585');
     const other = reaction.message.guild.roles.cache.find(guild => guild.id === '853630826284449803');
-    // const TT = reaction.guild.roles.cache.get("853628984415748146");
-    // const SH = reaction.guild.roles.cache.get("853630661265195028");
-    // const HH = reaction.guild.roles.cache.get("853630766746435585");
-    // const other = reaction.guild.roles.cache.get("853630826284449803");
+
 	// When a reaction is received, check if the structure is partial
 	if (reaction.partial) {
 		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
@@ -57,7 +54,8 @@ client.on('ready', () => {
 			// Return as `reaction.message.author` may be undefined/null
 			return;
 		}
-	}
+    }
+    
     console.log(reaction.message.id); // 853645612792152126  853645612792152126 853689102163771432
     if (reaction.message.author.id == '853624054886694932' && reaction.message.id == '853645612792152126') {
         if (reaction._emoji.name == '1️⃣') {
@@ -82,6 +80,54 @@ client.on('ready', () => {
             console.log(user);
             reaction.message.guild.members.fetch(user.id).then(member => {
                 member.roles.add(other).catch(console.error);
+            })
+        }
+    }
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+    // cached roles to be removed
+    const TT = reaction.message.guild.roles.cache.find(guild => guild.id === '853628984415748146');
+    const SH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630661265195028');
+    const HH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630766746435585');
+    const other = reaction.message.guild.roles.cache.find(guild => guild.id === '853630826284449803');
+
+	// When a reaction is received, check if the structure is partial
+    if (reaction.partial) {
+		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+    }
+    
+    console.log(reaction); // 853645612792152126  853645612792152126 853689102163771432
+    if (reaction.message.author.id == '853624054886694932' && reaction.message.id == '853645612792152126') {
+        if (reaction._emoji.name == '1️⃣') {
+            console.log(user);
+            reaction.message.guild.members.fetch(user.id).then(member => {
+                member.roles.remove(TT).catch(console.error);
+            })
+        }
+        if (reaction._emoji.name == '2️⃣') {
+            console.log(user);
+            reaction.message.guild.members.fetch(user.id).then(member => {
+                member.roles.remove(SH).catch(console.error);
+            })
+        }
+        if (reaction._emoji.name == '3️⃣') {
+            console.log(user);
+            reaction.message.guild.members.fetch(user.id).then(member => {
+                member.roles.remove(HH).catch(console.error);
+            })
+        }
+        if (reaction._emoji.name == '4️⃣') {
+            console.log(user);
+            reaction.message.guild.members.fetch(user.id).then(member => {
+                member.roles.remove(other).catch(console.error);
             })
         }
     }
