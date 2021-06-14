2869caeb0ca03ace9f475aa0e1f7b9bb4a95486a
Ellie <37948990+Elisiah@users.noreply.github.com>
2021-06-14 08:51:00 +0100

    QoL
    - Made commonly used IDs consts so the bot can more easily be locally tested
    - Added some comments and extra info

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+// ExLean Bot - For the Exeter University Lean Proofs Server
+// Author: James A, Contributions: Ellie V
+
 require('dotenv').config()
 
 const express = require('express');
@@ -7,18 +10,31 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello World'));
 app.listen(port, () => console.log('Example App'));
 
-// =============
+// ====== Discord Constants =======
 
+// discord js setup
 const Discord = require('discord.js');
 const { Client, MessageEmbed } = require('discord.js');
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
+// discord id codes
+const theyThemRole = '853628984415748146';
+const sheHerRole = '853630661265195028';
+const heHimRole = '853630766746435585';
+const otherRole = '853630826284449803';
+const botUserId = '853624054886694932';
+const reactionMessage = '853645612792152126';
+
+// ====== Bot Code =======
+
+// startup
 client.login(process.env.DISCORD_TOKEN)
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-  
+
+// messages and responses
 client.on('message', msg => {
     if (msg.content.toLowerCase().includes('ping')) {
       msg.channel.send('Pong!');
@@ -37,12 +53,13 @@ client.on('message', msg => {
   }
 });
 
+// give roles system
 client.on('messageReactionAdd', async (reaction, user) => {
     // cached roles to be added
-    const TT = reaction.message.guild.roles.cache.find(guild => guild.id === '853628984415748146');
-    const SH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630661265195028');
-    const HH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630766746435585');
-    const other = reaction.message.guild.roles.cache.find(guild => guild.id === '853630826284449803');
+    const TT = reaction.message.guild.roles.cache.find(guild => guild.id === theyThemRole);
+    const SH = reaction.message.guild.roles.cache.find(guild => guild.id === sheHerRole);
+    const HH = reaction.message.guild.roles.cache.find(guild => guild.id === heHimRole);
+    const other = reaction.message.guild.roles.cache.find(guild => guild.id === otherRole);
 
 	// When a reaction is received, check if the structure is partial
 	if (reaction.partial) {
@@ -56,8 +73,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
 		}
     }
     
-    console.log(reaction.message.id); // 853645612792152126  853645612792152126 853689102163771432
-    if (reaction.message.author.id == '853624054886694932' && reaction.message.id == '853645612792152126') {
+    if (reaction.message.author.id == botUserId && reaction.message.id == reactionMessage) {
         if (reaction._emoji.name == '1️⃣') {
             console.log(user);
             reaction.message.guild.members.fetch(user.id).then(member => {
@@ -85,12 +101,13 @@ client.on('messageReactionAdd', async (reaction, user) => {
     }
 });
 
+// remove roles system
 client.on('messageReactionRemove', async (reaction, user) => {
     // cached roles to be removed
-    const TT = reaction.message.guild.roles.cache.find(guild => guild.id === '853628984415748146');
-    const SH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630661265195028');
-    const HH = reaction.message.guild.roles.cache.find(guild => guild.id === '853630766746435585');
-    const other = reaction.message.guild.roles.cache.find(guild => guild.id === '853630826284449803');
+    const TT = reaction.message.guild.roles.cache.find(guild => guild.id === theyThemRole);
+    const SH = reaction.message.guild.roles.cache.find(guild => guild.id === sheHerRole);
+    const HH = reaction.message.guild.roles.cache.find(guild => guild.id === heHimRole);
+    const other = reaction.message.guild.roles.cache.find(guild => guild.id === otherRole);
 
 	// When a reaction is received, check if the structure is partial
     if (reaction.partial) {
@@ -103,9 +120,8 @@ client.on('messageReactionRemove', async (reaction, user) => {
 			return;
 		}
     }
-    
-    console.log(reaction); // 853645612792152126  853645612792152126 853689102163771432
-    if (reaction.message.author.id == '853624054886694932' && reaction.message.id == '853645612792152126') {
+
+    if (reaction.message.author.id == botUserId && reaction.message.id == reactionMessage) {
         if (reaction._emoji.name == '1️⃣') {
             console.log(user);
             reaction.message.guild.members.fetch(user.id).then(member => {
